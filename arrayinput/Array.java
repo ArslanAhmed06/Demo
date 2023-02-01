bdb035e567af6be55b5d1435b52fe88d7beac60b
Arslan <arslanahmedbhatti99@gmail.com>
2023-01-31 16:29:25 -0800

    Remove some lines

--- arrayinput/Array.java
+++ arrayinput/Array.java
@@ -6,10 +6,6 @@ public class Array {
 		int [] numbers = {10,20,30,40,50};
 		for(int i : numbers)
 		System.out.println(i);
-		
-		String[][] Random = { {"Apple","Mango","Yogurt","slice"}, {"Biscuit","Juice","Bottle"} };
-		System.out.println(Random[1][1]);
-		
 		int [][] cars = {{1,2,3,4}, {5,6,7}};
 		for(int i=0; i<cars.length;++i) {
 			for(int j=0; j<cars[i].length; ++j) {
